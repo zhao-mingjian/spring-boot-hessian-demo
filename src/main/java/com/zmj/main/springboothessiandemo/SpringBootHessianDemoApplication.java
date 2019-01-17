d014da6e36a913b692ca5c49443d830b016a388b
1353095373@qq.com <zmj950804>
2019-01-17 17:42:21 +0800

    第一次提交

---
+++ src/main/java/com/zmj/main/springboothessiandemo/SpringBootHessianDemoApplication.java
@@ -0,0 +1,14 @@
+package com.zmj.main.springboothessiandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHessianDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHessianDemoApplication.class, args);
+	}
+
+}
+
